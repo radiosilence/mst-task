b35ef9b3785e53b959e1fa46f401071ebd32aaa4
James Cleveland <jc@blit.cc>
2021-04-09 11:00:36 +0100

    bring most of model out of factory

--- src/models.ts
+++ src/models.ts
@@ -9,60 +9,61 @@ export function randomHex(): string {
 
 export const TaskState = enumeration(["ready", "inProgress", "done", "failed"]);
 
+const Task = model("Task")
+  .props({
+    state: optional(TaskState, "ready"),
+    error: frozen(),
+  })
+  .volatile(() => ({
+    executionId: randomHex(),
+  }))
+  .views(self => ({
+    get ready() {
+      return self.state === "ready";
+    },
+    get inProgress() {
+      return self.state === "inProgress";
+    },
+    get success() {
+      return self.state === "done";
+    },
+    get failed() {
+      return self.state === "failed";
+    },
+  }))
+  .actions(self => {
+    function reset() {
+      self.state = "ready";
+      self.executionId = randomHex();
+    }
+
+    return {
+      reset,
+    };
+  });
+
 export function taskFrom<Value, Args extends unknown[]>(
   cb: AsyncFn<Value, Args>,
 ) {
-  return model("Task")
-    .props({
-      state: optional(TaskState, "ready"),
-      error: frozen(),
-    })
-    .volatile(() => ({
-      executionId: randomHex(),
-    }))
-    .views(self => ({
-      get ready() {
-        return self.state === "ready";
-      },
-      get inProgress() {
-        return self.state === "inProgress";
-      },
-      get success() {
-        return self.state === "done";
-      },
-      get failed() {
-        return self.state === "failed";
-      },
-    }))
-    .actions(self => {
-      function reset() {
-        self.state = "ready";
-        self.executionId = randomHex();
-      }
-
-      const execute = toGeneratorFunction(
-        flow(function* (...args: Args) {
-          try {
-            reset();
-            const executionId = self.executionId;
-            self.state = "inProgress";
-            const value = yield* toGenerator(cb(...args));
-            if (self.executionId !== executionId) {
-              return [value, true] as Result<Value>;
-            }
-            self.state = "done";
-            return [value, false] as Result<Value>;
-          } catch (error) {
-            self.state = "failed";
-            self.error = error;
-            throw error;
+  return Task.actions(self => ({
+    execute: toGeneratorFunction(
+      flow(function* (...args: Args) {
+        try {
+          self.reset();
+          const executionId = self.executionId;
+          self.state = "inProgress";
+          const value = yield* toGenerator(cb(...args));
+          if (self.executionId !== executionId) {
+            return [value, true] as Result<Value>;
           }
-        }),
-      );
-
-      return {
-        reset,
-        execute,
-      };
-    });
+          self.state = "done";
+          return [value, false] as Result<Value>;
+        } catch (error) {
+          self.state = "failed";
+          self.error = error;
+          throw error;
+        }
+      }),
+    ),
+  }));
 }
--- src/types.ts
+++ src/types.ts
@@ -5,15 +5,3 @@ export enum ResultStatus {
 export type AsyncFn<T, Args extends unknown[]> = (...args: Args) => Promise<T>;
 
 export type Result<R> = [value: R, stale: boolean];
-
-// export class Result<R> {
-//   value: R;
-//   stale: boolean;
-
-//   constructor(value: R, stale: boolean) {
-//     this.value = value;
-//     this.stale = stale;
-//   }
-
-//   ok: true = true;
-// }
